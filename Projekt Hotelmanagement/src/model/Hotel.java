e5d9a154d15d04e7ae2742aaabfdfa593b45b46e
Rewa <psycho_droge@gmx.de>
2012-11-29 12:34:47 +0100

    Repository ist Online.

---
+++ Projekt Hotelmanagement/src/model/Hotel.java
@@ -0,0 +1,21 @@
+package model;
+
+import javgui.views.MainGui;
+
+
+//Zum Ausführen der GUI
+public class Hotel {
+	
+	
+	public static void main (String args []){
+		try{
+			MainGui.guishow();
+		}catch(Exception e){
+			System.out.println("Fehler");
+		}
+		
+		
+		
+	}
+
+}
